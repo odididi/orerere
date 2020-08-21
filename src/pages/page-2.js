63272683533fa51ffdd2b745185fddd64a80b306
Odysseas Zafeiriou <odysseas@wearereasonablepeople.com>
2020-08-21 11:10:43 +0200

    Add contentful

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+export const systemsQuery = graphql`
+  {
+    allContentfulSystem {
+      edges {
+        node {
+          type
+          slug
+          description
+          presentationVideo {
+            description
+            title
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const System = ({system}) => (
+  <div style={{background: '#d3d3d3', borderRadius: '4px', padding: '16px', display: 'flex', flexDirection: 'column'}}>
+    <h1>{system.type}</h1>
+    <p>{system.description}</p>
+    <video controls style={{outline: 'none', background: '#191414'}}>
+      <source src={system.presentationVideo.file.url} type="video/mp4" />
+    </video>
+  </div>
+);
+
+const SecondPage = ({data}) => {
+  const systems = data.allContentfulSystem.edges.map(e => e.node);
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      {systems.map(s => <System system={s} />)}
+    </Layout>
+  )
+}
 
 export default SecondPage
